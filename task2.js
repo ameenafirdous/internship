7ad718dcdf6d2fe38060305e20f3a793ce300711
ameenafirdous <amenafirdous7861@gmail.com>
2025-07-10 11:46:59 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("🌿 Thank you! We'll get back to you shortly.");
+  this.reset();
+});
